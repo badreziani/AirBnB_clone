6f8f35cfa6018a7b8d8907ae11bf5303f5accc52
ifiok1equere <equereifiok1@gmail.com>
2024-05-14 17:25:11 +0100

    task 4: update init method to instantiate from a dictionary if not empty or None

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,21 @@ import uuid
 class BaseModel:
     """This class defines a blueprint called BaseModel"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization method"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            del kwargs["__class__"]
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+
+                # set attributes of instance with dict keys/values parsed
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Unofficial representation of instance"""
