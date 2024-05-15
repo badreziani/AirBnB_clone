c889b2c31ccd6102c7ffdb5aaa30cf2d0a74380e
ifiok1equere <equereifiok1@gmail.com>
2024-05-15 12:20:42 +0100

    delete packages, add folder for snapshot of code for each task & update test method to include test for __str__ magic method

--- packages/my_math/functions/add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-"""code to add only two positive integers"""
-
-from my_math.positive import is_positive
-
-def my_add(a, b):
-    if is_positive(a) and is_positive(b):
-        return a + b
-    return "cant perform operation on negative numbers"
--- packages/my_math/positive.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-from .abs import my_abs
-
-def is_positive(n):
-    return my_abs(n) == n
