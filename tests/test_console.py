18bcd73293ed5af8f3843805d64d93d2d46c7dba
Badre Ziani <badreziani@gmail.com>
2024-05-19 20:43:00 +0100

    Create test for the console

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+test_console module - contains tests for the HBNBCommand class
+"""
+
+import unittest
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """TestHBNBCommand class
+    """
+
+    pass
