abe4c741f21ae6fedae20a75bd9dd465a55bb78c
ifiok1equere <equereifiok1@gmail.com>
2024-05-20 20:18:46 +0100

    resolve checker failure for help method

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,37 +7,23 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
-import sys
 
 
 class TestHBNBCommand(unittest.TestCase):
     """Unittests for all feautures of the Console"""
 
-    def setUp(self):
-        """Set up test environment"""
-        self.console = HBNBCommand()
-        self.std_out = StringIO()
-        sys.stdout = self.std_out
-
-    def tearDown(self):
-        """Tear down console object"""
-        self.std_out.close()
-        sys.stdout = sys.__stdout__
-
-    '''
-    def test_help(self):
-        """Test the help method"""
+    def test_help_show(self):
+        """Test help command"""
+        
+        output = "Prints the string representation of an instance\n"
 
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help show")
-
-        help_string = "Prints the string representation of an instance"
-
-        self.assertTrue(f.getvalue(), help_string)
-    '''
+            self.assertEqual(f.getvalue(), output)
 
     def test_quit(self):
         """Test quit command"""
 
-        self.console.onecmd("quit")
-        self.assertEqual(self.std_out.getvalue(), "")
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("quit")
+            self.assertEqual(f.getvalue(), "")
